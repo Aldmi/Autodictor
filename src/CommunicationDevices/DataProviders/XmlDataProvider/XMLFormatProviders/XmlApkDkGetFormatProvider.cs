7246e09b2071b2d78dfb215f63292163824ed443
Aldmi <Aldmi@bk.ru>
2017-09-08 14:41:05 +0700

    Change map trainType to "XmlFormatProvider"

---
+++ src/CommunicationDevices/DataProviders/XmlDataProvider/XMLFormatProviders/XmlApkDkGetFormatProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunicationDevices.DataProviders.XmlDataProvider.XMLFormatProviders
+{
+    public class XmlApkDkGetFormatProvider : IFormatProvider
+    {
+        public string CreateDoc(IEnumerable<UniversalInputType> tables)
+        {
+            return null;
+        }
+    }
+}
